c0e4042a0745e01560c3a81c76e0d3caf210dcea
Raul Burian <raul.burian@bca.com>
2021-06-17 12:40:35 +0300

    better look

--- AzureDemoFE/src/app/app.component.ts
+++ AzureDemoFE/src/app/app.component.ts
@@ -14,22 +14,30 @@ export class AppComponent {
   encodingKey;
   decodedText;
   show = false;
+  loadingUpload = false;
+  loadingDecode = false;
 
   constructor(private httpService: HttpService) {
   }
 
   uploadFile(): void {
+    this.loadingUpload = true;
     this.httpService.uploadContent({ email: this.email, content: this.fileRequestContent })
       .subscribe(_ => {
         this.show = true;
+        this.loadingUpload = false;
         setTimeout(() => {
           this.show = false;
         }, 3000);
-      });
+      }, _ => this.loadingUpload = false);
   }
 
   decodeText(): void {
+    this.loadingDecode = true;
     this.httpService.decodeContent({ content: this.encodedText, encodingKey: this.encodingKey })
-      .subscribe(decodedText => this.decodedText = decodedText);
+      .subscribe(decodedText => {
+        this.loadingDecode = false;
+        this.decodedText = decodedText;
+      }, _ => this.loadingDecode = false);
   }
 }
